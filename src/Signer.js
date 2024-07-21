8d58476e8e9dc187c73352d7c84dcd0c3e951b89
hhh <shshshsh@horsefucker.org>
2024-07-21 13:14:42 +0300

    change default useragent

--- src/Signer.js
+++ src/Signer.js
@@ -8,7 +8,7 @@ const { createCipheriv } = require("crypto");
 class Signer {
   /** Default user-agent used to signed the requests */
   static DEFAULT_USERAGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0";
   /** Password for x-tt-params AES enc/dec */
   static PASSWORD = "webapp1.0+202106";
 
